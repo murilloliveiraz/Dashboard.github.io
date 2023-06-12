518682c28627b0a93a1ada01117a7b8159dec887
murillo <murillo1047@gmail.com>
2023-06-12 17:52:26 -0300

    fim do projeto

---
+++ assets/js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus : 'Aguardando',
+        shipping : 'Pendente' 
+    },
+    {
+        productName : 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus : 'Devolvido',
+        shipping : 'Cancelado' 
+    },
+    {
+        productName : 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus : 'Aguardando',
+        shipping : 'Pendente' 
+    },
+    {
+        productName : 'Drone With Camera',
+        productNumber: '96996',
+        paymentStatus : 'Pago',
+        shipping : 'Entregue' 
+    },
+    {
+        productName : 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus : 'Pago',
+        shipping : 'Entregue' 
+    },
+    {
+        productName : 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus : 'Aguardando',
+        shipping : 'Pendente' 
+    },
+    {
+        productName : 'Lozengue Drone',
+        productNumber: '00482',
+        paymentStatus : 'Pago',
+        shipping : 'Entregue' 
+    },
+]
